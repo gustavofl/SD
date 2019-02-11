d53af693188aa401526d106bf06d35dd5a46e081
gustavofl <gustavofernandezluz@gmail.com>
2019-02-10 23:59:49 -0300

    Automatizacao na plotagem dos graficos

---
+++ script_gerar_graficos.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+## OBTER O MAIOR TEMPO FINAL DOS ARQUIVOS
+tempo_final=`tail -n1 mysqld.txt`
+if [ `tail -n1 oned.txt` -lt $tempo_final ]; then
+	tempo_final=`tail -n1 oned.txt`
+fi
+if [ `tail -n1 systemd.txt` -lt $tempo_final ]; then
+	tempo_final=`tail -n1 systemd.txt`
+fi
+
+data=`date -d@$tempo_final`
+dia=`echo $data | awk '{print $3}'`
+mes=`echo $data | awk '{print $2}'`
+hora=`echo $data | awk '{print $4}' | awk -F: '{print $1}'`
+minutos=`echo $data | awk '{print $4}' | awk -F: '{print $2}'`
+
+nome_pasta=`echo $dia-$mes $hora-$minutos`
+
+## Verificar se a pasta existe
+if [ -d "imagens/$nome_pasta" ]; then
+	echo "Ja existe uma pasta com imagens desses dados, deseja sobrescrever os arquivos?"
+	echo "Sobrescrevendo imagens."
+else
+	mkdir "imagens/$nome_pasta"
+fi
+
+rm ultimas_imagens
+ln -s "imagens/$nome_pasta" ultimas_imagens
+
+gnuplot "script gnuplot portugues.txt"
