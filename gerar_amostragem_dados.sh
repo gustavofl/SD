55dac496edf50876726da6b2c0893f1299580d14
gustavofl <gustavofernandezluz@gmail.com>
2019-02-20 11:18:32 -0300

    alteracao da escala de tempo de minutos para horas

--- gerar_amostragem_dados.sh
+++ gerar_amostragem_dados.sh
@@ -9,7 +9,7 @@ while read line
 do
 	amostra=`echo $line | awk '{print $1}'`
 	if [[ $amostra-$ultima_amostra -ge $intervalo_amostragem ]]; then 
-		minutos_amostra=`echo $amostra/60 | bc`
+		minutos_amostra=`echo $amostra/3600 | bc`
 		mem_virtual=`echo "scale=3;$(echo $line | awk '{print $2}')/1000" | bc`
 		mem_residente=`echo "scale=3;$(echo $line | awk '{print $3}')/1000" | bc`
 		mem_compartilhada=`echo "scale=3;$(echo $line | awk '{print $4}')/1000" | bc`
